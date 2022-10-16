f10533641445a2e455da61f23da2ce482e79896b
Tolik <ProtorskyAV@yandex.ru>
2022-10-16 19:52:00 +0300

    Add IoC pattern

--- controller.py
+++ controller.py
@@ -1,10 +1,28 @@
 from statistics import mode
-import  model_sum as model
+import model
 import view
 
 def button_click():
     value_a = view.get_value()
     value_b = view.get_value()
-    model.init(value_a, value_b)
-    result = model.do_it()
-    view.view_data(result, "resul")
+    value_o = view.get_opert()
+    
+    if 'i' in value_a and 'i' in value_b:
+        type_num = model.model_name_kmp
+    else:
+        type_num = model.model_name_rac
+
+    model.init(value_a, value_b, type_num)
+
+    if value_o == '*':
+        result = model.mult()
+    elif value_o == '/':
+        result = model.div()
+    elif value_o == '+':
+        result = model.sum()
+    elif value_o == '-':
+        result = model.sub()
+    else:
+        raise Exception('Operation not support!')            
+
+    view.view_data(result, "result")
--- model.py
+++ model.py
@@ -1,42 +1,45 @@
-
-import Komp
 import rac
-TYPE_NUM = None
+import kmp
+
+model_name_rac = 'rac'
+model_name_kmp = 'kmp'
+
+model_name_sum = 'sum'
+model_name_sub = 'sub'
+model_name_div = 'div'
+model_name_mlt = 'mult'
+model_name_int = 'init'
+
+model_ioc = dict()
 
 def init(in_a, in_b, type_num):
-    global TYPE_NUM
-    TYPE_NUM = type_num
-    if type_num:
-        Komp.init(in_a, in_b)
-        
+    if type_num not in (model_name_rac, model_name_kmp):
+        raise Exception('Model not support!')            
+
+    if type_num == midel_name_rac:
+        model_ioc[model_name_int] = rac.init
+        model_ioc[model_name_mlt] = rac.mult
+        model_ioc[model_name_sum] = rac.sum
+        model_ioc[model_name_sub] = rac.sub
     else:
-        rac.init(in_a, in_b)
+        model_ioc[model_name_int] = kmp.init
+        model_ioc[model_name_mlt] = kmp.mult
+        model_ioc[model_name_sum] = kmp.sum
+        model_ioc[model_name_sub] = kmp.sub
+
+    model_ioc.get(model_name_int)(in_a, in_b)
             
 def mult():
-    if TYPE_NUM:
-        return Komp.mult()   # указать функцию
-    else:
-        return rac.mult()
+    return model_ioc.get(model_name_mlt)   # указать функцию
 
 def sum():
-    if TYPE_NUM:
-        return Komp.sum()   # указать функцию
-    else:
-        return rac.sum()
+    return model_ioc.get(model_name_sum)   # указать функцию
 
 def div():
-    if TYPE_NUM:
-        return Komp.div()   # указать функцию
-    else:
-        return rac.div()
+    return model_ioc.get(model_name_div)   # указать функцию
 
 def sub():
-    if TYPE_NUM:
-        return Komp.sub()   # указать функцию
-    else:
-        return rac.sub()
-
-
+    return model_ioc.get(model_name_sub)   # указать функцию
 
 
 
